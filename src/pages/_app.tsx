a7d085dbc32a39ad1db1ca5a78f99c429038f2b6
novianto22 <99942803+novianto22@users.noreply.github.com>
2023-01-25 20:37:35 +0700

    Initial Setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import {
+    QueryClientProvider,
+    QueryClient,
+    Hydrate,
+} from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    // This ensures that data is not shared
+    // between different users and requests
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        staleTime: 1000 * 60 * 5,
+                    },
+                },
+            })
+    );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <Component {...pageProps} />
+            </Hydrate>
+        </QueryClientProvider>
+    );
 }
